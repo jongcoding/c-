059432d870971c50e099a03f31f8a86fb86846a7
JONG <128033808+jongcoding@users.noreply.github.com>
2025-04-13 11:56:58 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+
+
+        }
+    }
+}
