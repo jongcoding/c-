b674df8cf20972c01c3a6ae092881898907bfcd9
JONG <128033808+jongcoding@users.noreply.github.com>
2025-04-13 11:12:27 +0900

    [Bronze V] Title: ??!, Time: 44 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(input+ "??!");
+
+
+    }
+}
+
+
