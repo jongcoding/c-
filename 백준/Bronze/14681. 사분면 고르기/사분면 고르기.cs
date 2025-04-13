e2b27005a0d4c84abb5ea17cf7b494ac6d4ccbba
JONG <128033808+jongcoding@users.noreply.github.com>
2025-04-13 14:01:36 +0900

    [Bronze V] Title: 사분면 고르기, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,42 @@
+using System;
+
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        if (x>0)
+        {
+            if (y>0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(4);
+            }
+            
+        }
+        else
+        {
+            if (y>0)
+            {
+                Console.WriteLine(2);
+            }
+            else
+            {
+                Console.WriteLine(3);
+
+            }
+            
+        }
+       
+    }
+}
+
+
