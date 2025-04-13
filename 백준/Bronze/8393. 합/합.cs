c9a39234f9812c2e3a5b3fd0de5b634e690fe568
JONG <128033808+jongcoding@users.noreply.github.com>
2025-04-13 16:28:19 +0900

    [Bronze V] Title: 합, Time: 48 ms, Memory: 5460 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.cs
+++ 백준/Bronze/8393. 합/합.cs
@@ -8,12 +8,7 @@ class Program
     static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine());
-        int result = 0;
-        for (int i=1; i<n+1;i++)
-        {
-            result += i;
-        }
-        Console.WriteLine(result);
+        Console.WriteLine(n*(n+1)/2);
         
 
     }
