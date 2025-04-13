c449aaf1f835bb1dfa2e2d46ea3102b173912ee5
JONG <128033808+jongcoding@users.noreply.github.com>
2025-04-13 11:58:44 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+
+
+        }
+    }
+}
