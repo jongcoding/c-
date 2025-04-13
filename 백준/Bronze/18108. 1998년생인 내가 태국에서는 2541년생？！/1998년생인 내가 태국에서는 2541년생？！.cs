ff21524553953bce16fb37f884baa88d28500411
JONG <128033808+jongcoding@users.noreply.github.com>
2025-04-13 11:16:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int input =int.Parse(Console.ReadLine());
+        Console.WriteLine(input- 543);
+
+
+    }
+}
+
+
